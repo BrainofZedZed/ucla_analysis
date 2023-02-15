0fd1190bb54cda15168fab393b0a60ec1eb8178e
Zach Zeidler <zezeidler@gmail.com>
2023-02-14 16:52:05 -0800

    changed from baseline 10s to recall 30s

--- fiber photometry/FP_noBD_analysis_baselinetoneslaser.m
+++ fiber photometry/FP_noBD_analysis_baselinetoneslaser.m
@@ -17,14 +17,16 @@ for i = 1:length(folderList)
 end
 
 % define pre and post times to visualize
-t_pre = 2;
-t_post =2;
-tvec = [0-t_pre 10+t_post];
+t_pre = 5;
+t_post = 5;
+cs_dur = 30;
+t_total = t_pre+t_post+cs_dur;
+tvec = [0-t_pre t_total];
 
 % get TDT data, filtered
 tdt_dir = folderList{tdt_f};
 data = TDTbin2mat(tdt_dir, 'TYPE', {'epocs', 'scalars', 'streams'});
-datafilt = TDTfilter(data, 'PC1/','TIME', [-5, 40]);
+datafilt = TDTfilter(data, 'PC0/','TIME', tvec);
 
 tdt_fs = datafilt.streams.x465A.fs;
 
@@ -75,15 +77,15 @@ plot(mean(sig_nolaser,1), 'LineWidth', 2, 'Color', 'black');
 xline(tdt_fs*t_pre, 'r:');
 xl2 = size(sig,2);
 xl2 = xl2 - (tdt_fs*t_post);
-xline(xl2, 'r:')
-xtx = [0:2:14];
+xline(xl2, 'r:');
+xtx = [0:5:40];
 xtx2 = (xtx*tdt_fs);
 xticks(xtx2);
-xticklabels({'-2','0','2','4','6','8','10', '12'});
-title('baseline tone2 (12kHz) response -- no laser')
+xticklabels({'-5','0','5','10','15','20','25', '30', '35', '40'});
+title('tone (CS+) response -- no laser')
 legend('tone1', 'tone2', 'tone3', 'tone4', 'mean');
 xlabel('time from tone onset')
-savefig('baseline_tones2_noLaser.fig');
+savefig('D1_CSp_tonesResponse_noLaser.fig');
 
 figure;
 hold on;
@@ -95,17 +97,18 @@ plot(mean(sig_laser,1), 'LineWidth', 2, 'Color', 'black');
 xline(tdt_fs*t_pre, 'r:');
 xl2 = size(sig,2);
 xl2 = xl2 - (tdt_fs*t_post);
-xline(xl2, 'r:')
-xtx = [0:2:14];
+xline(xl2, 'r:');
+xtx = [0:5:40];
 xtx2 = (xtx*tdt_fs);
 xticks(xtx2);
-xticklabels({'-2','0','2','4','6','8','10', '12'});
-title('baseline tone2 (12kHz) response -- laser')
+xticklabels({'-5','0','5','10','15','20','25', '30', '35', '40'});
+title('tone (CS+) response -- laser')
 legend('tone1', 'tone2', 'tone3', 'tone4', 'mean');
 xlabel('time from tone+laser onset')
-savefig('baseline_tones2_Laser.fig');
+ylabel('zscore');
+savefig('D1_CSp_toneResponse_Laser.fig');
 
-save('baseline_sig2_laser.mat')
+save('D1_CSp_toneResponse.mat')
 
 %% code graveyard
 % tdt_start = datevec(datafilt.info.utcStartTime);
