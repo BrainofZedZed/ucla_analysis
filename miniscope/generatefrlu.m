493fb1709a8939097b8ba54788bce75d03341637
Zach Zeidler <zezeidler@gmail.com>
2023-05-04 11:59:15 -0700

    update reading reward arduino data script

--- miniscope/generatefrlu.m
+++ miniscope/generatefrlu.m
@@ -1,8 +1,10 @@
-% generate frame lookup (frlu) and frame timestamp lookup (frtslu) files
-% from miniscope data organized as Parent > Day... > Animal > [standard
-% miniscope folders].
+% USE: generate frame lookup (frlu) and frame timestamp lookup (frtslu) files
+% SETUP: run file, select folder containing multiple days of miniscope
+% recordings. Each day is assumed to have the structure [Day] > [Time] >
+% [My_V4_Miniscope] and [My_Webcam]
+% OUTPUT: saves two files, frlu.mat and frtslu.mat, in experiment folder
 
-choice = uigetdir;
+choice = uigetdir('','Select directory above all other batched directories to generate frame lookup table');
 cd(choice);
 top_dir = dir;%('','Select the directory containing folders for analysis'); %Directory with list of folders containing videos + tracking to analyze
 
@@ -14,9 +16,9 @@ for i = 3:length(top_dir)
     for j = 3:length(idir)
         cd(idir(j).name);
         a = dir;
-        cd(a(3).name);
-        a = dir;
-        cd(a(3).name);
+        %cd(a(3).name);
+        %a = dir;
+        %cd(a(3).name);
         cd('My_V4_Miniscope')
         mstbl = readmatrix('timeStamps.csv');
         mstbl = mstbl(2:end,:);
